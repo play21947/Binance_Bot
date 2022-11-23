586f3ef168bad406651725aa9844b69132dbf359
play21947 <play21947@gmail.com>
2022-11-23 13:17:05 +0700

    add Server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const mysql2 = require('mysql2')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+const dbcon = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'binance'
+})
+
+app.listen(3001, ()=>{
+    console.log("Server is running on port 3001")
+})
